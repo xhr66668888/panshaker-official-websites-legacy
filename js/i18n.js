fc909f4180d6143d215f521dd1364c1a336539a8
Haoran Xu <xuhaoran6993@gmail.com>
2026-04-22 01:30:10 +0900

    sale

--- js/i18n.js
+++ js/i18n.js
@@ -661,7 +661,7 @@
         }
 
         var basePath = getBasePath();
-        var url = basePath + 'js/lang/' + lang + '.json?v=4';
+        var url = basePath + 'js/lang/' + lang + '.json?v=6';
 
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
